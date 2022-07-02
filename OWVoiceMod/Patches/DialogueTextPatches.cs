802a66b5f44632eb0cb207ca20d4de1a3c946482
Krevace <gtbunn23@student.capehenry.org>
2022-07-02 17:32:18 -0400

    Namespace consistency.

--- OWVoiceMod/Patches/DialogueTextPatches.cs
+++ OWVoiceMod/Patches/DialogueTextPatches.cs
@@ -1,21 +1,20 @@
 ﻿using System.Collections.Generic;
 
-namespace OWVoiceMod.Patches
-{
-	public class DialogueTextPatches
-    {
-		public static bool GetDisplayStringList(DialogueText __instance, ref List<string> __result)
-		{
-			// Ensures that the correct ID is used if dialogue uses randomization (ex. Gabbro intro lines)
-			if (__instance._randomize)
-			{
-				OWVoiceMod.randomDialogueNum = UnityEngine.Random.Range(0, __instance._listTextBlocks.Count);
-				__result = __instance._listTextBlocks[OWVoiceMod.randomDialogueNum].listPageText;
-				return false;
-			}
+namespace OWVoiceMod.Patches;
 
-			OWVoiceMod.randomDialogueNum = -1;
-			return true;
+public class DialogueTextPatches
+{
+	public static bool GetDisplayStringList(DialogueText __instance, ref List<string> __result)
+	{
+		// Ensures that the correct ID is used if dialogue uses randomization (ex. Gabbro intro lines)
+		if (__instance._randomize)
+		{
+			OWVoiceMod.randomDialogueNum = UnityEngine.Random.Range(0, __instance._listTextBlocks.Count);
+			__result = __instance._listTextBlocks[OWVoiceMod.randomDialogueNum].listPageText;
+			return false;
 		}
+
+		OWVoiceMod.randomDialogueNum = -1;
+		return true;
 	}
 }
--- OWVoiceMod/Patches/NomaiTranslatorPropPatches.cs
+++ OWVoiceMod/Patches/NomaiTranslatorPropPatches.cs
@@ -1,78 +1,77 @@
-﻿namespace OWVoiceMod.Patches
-{
-	public class NomaiTranslatorPropPatches
+﻿namespace OWVoiceMod.Patches;
+
+public class NomaiTranslatorPropPatches
     {
-		private static string currentTextName;
-		private static string oldTextName;
+	private static string currentTextName;
+	private static string oldTextName;
 
-		public static void DisplayTextNode(NomaiTranslatorProp __instance)
+	public static void DisplayTextNode(NomaiTranslatorProp __instance)
+	{
+		var nomaiText = __instance._nomaiTextComponent;
+		var currentTextID = __instance._currentTextID;
+
+		string currentAssetName;
+		if (nomaiText is NomaiComputer or NomaiVesselComputer)
 		{
-			var nomaiText = __instance._nomaiTextComponent;
-			var currentTextID = __instance._currentTextID;
-
-			string currentAssetName;
-			if (nomaiText is NomaiComputer or NomaiVesselComputer)
-			{
-				if (!OWVoiceMod.nomaiComputers) return;
-				if (nomaiText.gameObject.TryGetComponent<NomaiWarpComputerLogger>(out _)) currentAssetName = "NomaiWarpComputer";
-				else currentAssetName = nomaiText._nomaiTextAsset.name;
-				currentTextName = $"{currentAssetName} {currentTextID}";
-			}
-			else
-			{
-				if (!OWVoiceMod.nomaiScrolls && nomaiText is NomaiWallText) return;
-				if (!OWVoiceMod.nomaiRecordings && nomaiText is not NomaiWallText) return;
-				currentAssetName = nomaiText._nomaiTextAsset.name;
-				currentTextName = $"{currentAssetName} {currentTextID}";
-			}
-
-			if (currentTextName == oldTextName) return;
-
-			OWVoiceMod.UnloadAudio();
-
-			if (nomaiText.IsTranslated(currentTextID))
-			{
-				OWVoiceMod.LoadAudio(currentTextName);
-				oldTextName = currentTextName;
-			}
-			else
-			{
-				oldTextName = null;
-			}
+			if (!OWVoiceMod.nomaiComputers) return;
+			if (nomaiText.gameObject.TryGetComponent<NomaiWarpComputerLogger>(out _)) currentAssetName = "NomaiWarpComputer";
+			else currentAssetName = nomaiText._nomaiTextAsset.name;
+			currentTextName = $"{currentAssetName} {currentTextID}";
+		}
+		else
+		{
+			if (!OWVoiceMod.nomaiScrolls && nomaiText is NomaiWallText) return;
+			if (!OWVoiceMod.nomaiRecordings && nomaiText is not NomaiWallText) return;
+			currentAssetName = nomaiText._nomaiTextAsset.name;
+			currentTextName = $"{currentAssetName} {currentTextID}";
 		}
 
-		public static void SetTargetingGhostText(NomaiTranslatorProp __instance, bool isTargetingGhostText)
-		{
-			if (__instance._isTargetingGhostText == isTargetingGhostText) return;
-			if (OWVoiceMod.owlkWriting && isTargetingGhostText)
-			{
-				OWVoiceMod.UnloadAudio();
-				OWVoiceMod.audioSource.loop = true;
-				OWVoiceMod.LoadAudio("OwlkStatic");
-			}
-		}
+		if (currentTextName == oldTextName) return;
 
-		public static void SetTooCloseToTarget(NomaiTranslatorProp __instance, bool value)
-		{
-			if (__instance._isTooCloseToTarget == value) return;
-			if (value)
-			{
-				OWVoiceMod.UnloadAudio();
-				oldTextName = null;
-			}
-		}
+		OWVoiceMod.UnloadAudio();
 
-		public static void ClearNomaiText(NomaiTranslatorProp __instance)
+		if (nomaiText.IsTranslated(currentTextID))
 		{
-			if (__instance._nomaiTextComponent == null) return;
-			OWVoiceMod.UnloadAudio();
+			OWVoiceMod.LoadAudio(currentTextName);
+			oldTextName = currentTextName;
+		}
+		else
+		{
 			oldTextName = null;
 		}
+	}
 
-		public static void OnUnequipTool()
+	public static void SetTargetingGhostText(NomaiTranslatorProp __instance, bool isTargetingGhostText)
+	{
+		if (__instance._isTargetingGhostText == isTargetingGhostText) return;
+		if (OWVoiceMod.owlkWriting && isTargetingGhostText)
+		{
+			OWVoiceMod.UnloadAudio();
+			OWVoiceMod.audioSource.loop = true;
+			OWVoiceMod.LoadAudio("OwlkStatic");
+		}
+	}
+
+	public static void SetTooCloseToTarget(NomaiTranslatorProp __instance, bool value)
+	{
+		if (__instance._isTooCloseToTarget == value) return;
+		if (value)
 		{
 			OWVoiceMod.UnloadAudio();
 			oldTextName = null;
 		}
+	}
+
+	public static void ClearNomaiText(NomaiTranslatorProp __instance)
+	{
+		if (__instance._nomaiTextComponent == null) return;
+		OWVoiceMod.UnloadAudio();
+		oldTextName = null;
+	}
+
+	public static void OnUnequipTool()
+	{
+		OWVoiceMod.UnloadAudio();
+		oldTextName = null;
 	}
 }
